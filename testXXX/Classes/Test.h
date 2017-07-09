aa3025a5caa3487e1ef771c583eb3056f5de379d
mojing <a646538908@126.com>
2017-07-09 09:54:48 +0800

    x

---
+++ testXXX/Classes/Test.h
@@ -0,0 +1,15 @@
+//
+//  Test.h
+//  testXXX
+//
+//  Created by lumingliang on 17/7/7.
+//  Copyright © 2017年 lumingliang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
++(void)log;
+
+@end
---
+++ testXXX/Classes/Test.m
@@ -0,0 +1,18 @@
+//
+//  Test.m
+//  testXXX
+//
+//  Created by lumingliang on 17/7/7.
+//  Copyright © 2017年 lumingliang. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
++(void)log{
+    NSLog(@"1");
+    NSLog(@"2");
+}
+
+@end
